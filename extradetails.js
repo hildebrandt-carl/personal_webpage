9926818de105a3891f9633de9f56eda7a3f97503
Carl <hildebrandt.carl@outlook.com>
2018-10-03 17:41:56 -0400

    lots of changes

--- extradetails.js
+++ extradetails.js
@@ -1,10 +1,10 @@
 // this file contains the data we need for the animal viewer
 
-var animals_data = {"animals":[
+var work_data = {"project":[
 	{
 		image1 : "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Coast_Garter_Snake.jpg/500px-Coast_Garter_Snake.jpg",
 		image2 : "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Nerodia_sipedon_shedding.JPG/440px-Nerodia_sipedon_shedding.JPG",
-		name : "Snake",
+		name : "Programmable CPU Design",
 		description : "Snakes are elongated, legless, carnivorous reptiles of the suborder Serpentes that can be distinguished from legless lizards by their lack of eyelids and external ears. Like all squamates, snakes are ectothermic, amniote vertebrates covered in overlapping scales. Many species of snakes have skulls with several more joints than their lizard ancestors, enabling them to swallow prey much larger than their heads with their highly mobile jaws. To accommodate their narrow bodies, snakes' paired organs (such as kidneys) appear one in front of the other instead of side by side, and most have only one functional lung. Some species retain a pelvic girdle with a pair of vestigial claws on either side of the cloaca. Living snakes are found on every continent except Antarctica, and on most smaller land masses — exceptions include some large islands, such as Ireland and New Zealand, and many small islands of the Atlantic and central Pacific.[3] Additionally, sea snakes are widespread throughout the Indian and Pacific Oceans. More than 20 families are currently recognized, comprising about 500 genera and about 3,400 species.[4][5] They range in size from the tiny, 10 cm-long thread snake to the reticulated python of up to 6.95 meters (22.8 ft) in length.[6] The fossil species Titanoboa cerrejonensis was 13 meters (43 ft) long.[7] Snakes are thought to have evolved from either burrowing or aquatic lizards, perhaps during the Jurassic period, with the earliest known fossils dating to between 143 and 167 Ma ago.[8] The diversity of modern snakes appeared during the Paleocene period (c 66 to 56 Ma ago). The oldest preserved descriptions of snakes can be found in the Brooklyn Papyrus. Most species are nonvenomous and those that have venom use it primarily to kill and subdue prey rather than for self-defense. Some possess venom potent enough to cause painful injury or death to humans. Nonvenomous snakes either swallow prey alive or kill by constriction.",
 		item: 1,
 		youtubelink: "https://www.youtube.com/embed/z9Ul9ccDOqE"
@@ -12,7 +12,7 @@ var animals_data = {"animals":[
 	{
 		image1 : "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Crocodilia_collage.jpg/600px-Crocodilia_collage.jpg",
 		image2 : "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Caiman_crocodilus_Tropicario_2.JPG/440px-Caiman_crocodilus_Tropicario_2.JPG",
-		name : "Crocodilia",
+		name : "First Principles Following Line Robot",
 		description : "The Crocodilia (or Crocodylia) are an order of large, predatory, semiaquatic reptiles. They appeared 83.5 million years ago in the Late Cretaceous period (Campanian stage) and are the closest living relatives of birds, as the two groups are the only known survivors of the Archosauria. Members of the crocodilian total group, the clade Pseudosuchia, appeared about 250 million years ago in the Early Triassic period, and diversified during the Mesozoic era. The order Crocodilia includes the true crocodiles (family Crocodylidae), the alligators and caimans (family Alligatoridae), and the gharial and false gharial (family Gavialidae). Although the term 'crocodiles' is sometimes used to refer to all of these, a less ambiguous vernacular term for this group is crocodilians.",
 		item: 2,
 		youtubelink: "https://www.youtube.com/embed/z9Ul9ccDOqE"
@@ -20,7 +20,7 @@ var animals_data = {"animals":[
 	{
 		image1 : "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Bartagame_fcm.jpg/500px-Bartagame_fcm.jpg",
 		image2 : "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Lizard_in_Yemen.JPG/440px-Lizard_in_Yemen.JPG",
-		name : "Lizard",
+		name : "Light and Cooling System Controlled Over A GSM Network",
 		description : "Lizards are a widespread group of squamate reptiles, with approximately over 6,000 species,[1] ranging across all continents except Antarctica, as well as most oceanic island chains. The group, traditionally recognized as the suborder Lacertilia, is defined as all extant members of the Lepidosauria (reptiles with overlapping scales) that are neither sphenodonts (i.e., tuatara) nor snakes – they form an evolutionary grade.[2] While the snakes are recognized as falling phylogenetically within the Toxicofera clade from which they evolved, the sphenodonts are the sister group to the squamates, the larger monophyletic group, which includes both the lizards and the snakes.",
 		item: 3,
 		youtubelink: "https://www.youtube.com/embed/z9Ul9ccDOqE"
@@ -28,7 +28,7 @@ var animals_data = {"animals":[
 	{
 		image1 : "https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Chelonia_mydas_is_going_for_the_air.jpg/440px-Chelonia_mydas_is_going_for_the_air.jpg",
 		image2 : "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Turtle3m.JPG/500px-Turtle3m.JPG",
-		name : "Turtle",
+		name : "Network Enabled Software Graphical Board Game",
 		description : "Turtles are reptiles of the order Testudines (or Chelonii[3]) characterised by a special bony or cartilaginous shell developed from their ribs and acting as a shield.[4] \"Turtle\" may refer to the order as a whole (American English) or to fresh-water and sea-dwelling testudines (British English). The order Testudines includes both extant (living) and extinct species. The earliest known members of this group date from 157 million years ago,[1] making turtles one of the oldest reptile groups and a more ancient group than snakes or crocodilians. Of the 327 known species alive today, some are highly endangered.",
 		item: 4,
 		youtubelink: "https://www.youtube.com/embed/z9Ul9ccDOqE"
@@ -37,7 +37,7 @@ var animals_data = {"animals":[
 	{
 		image1 : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/AfricanWildCat.jpg/440px-AfricanWildCat.jpg",
 		image2 : "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Kittyply_edit1.jpg/440px-Kittyply_edit1.jpg",
-		name : "Cat",
+		name : "Driver Mobile Phone Use Detection and Notification",
 		description : "The domestic cat (Felis catus or Felis silvestris catus) is a small, usually furry, domesticated, and carnivorous mammal. They are often called housecats when kept as an indoor pet or simply cats when there is no need to distinguish them from other felids and felines.[6] Cats are often valued by humans for companionship and their ability to hunt vermin.",
 		item: 5,
 		youtubelink: "https://www.youtube.com/embed/z9Ul9ccDOqE"
@@ -45,49 +45,59 @@ var animals_data = {"animals":[
 	{
 		image1 : "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Poligraf_Poligrafovich.JPG/340px-Poligraf_Poligrafovich.JPG",
 		image2 : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Cavalier_King_Charles_Spaniel_trio.jpg/440px-Cavalier_King_Charles_Spaniel_trio.jpg",
-		name : "Dog",
+		name : "NodeJS Server Integration with Android and Web Platform",
 		description : "The domestic dog (Canis lupus familiaris or Canis familiaris) is a domesticated canid which has been selectively bred for millennia for various behaviors, sensory capabilities, and physical attributes. Although initially thought to have originated as a manmade variant of an extant canid species (variously supposed as being the dhole, golden jackal, or gray wolf), extensive genetic studies undertaken during the 2010s indicate that dogs diverged from other wolf-like canids in Eurasia 40,000 years ago. Being the oldest domesticated animals, their long association with people has allowed dogs to be uniquely attuned to human behavior, as well as thrive on a starch-rich diet which would be inadequate for other canid species.",
 		item: 6,
 		youtubelink: "https://www.youtube.com/embed/z9Ul9ccDOqE"
 	},
 	{
-		image1 : "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Diademed_ready_to_push_off.jpg/440px-Diademed_ready_to_push_off.jpg",
-		image2 : "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Three_chimpanzees_with_apple.jpg/640px-Three_chimpanzees_with_apple.jpg",
-		name : "Primate",
-		description : "A primate is a mammal of the order Primates (Listeni/praɪˈmeɪtiːz/ pry-may-teez; Latin: \"prime, first rank\"). In taxonomy, primates include two distinct lineages, strepsirrhines and haplorhines. Primates arose from ancestors that lived in the trees of tropical forests; many primate characteristics represent adaptations to life in this challenging three-dimensional environment. Most primate species remain at least partly arboreal. With the exception of humans, who inhabit every continent, most primates live in tropical or subtropical regions of the Americas, Africa and Asia.[4] They range in size from Madame Berthe's mouse lemur, which weighs only 30 g (1 oz), to the eastern lowland gorilla, weighing over 200 kg (440 lb). Based on fossil evidence, the earliest known true primates, represented by the genus Teilhardina, date to 55.8 million years old.[5] An early close primate relative known from abundant remains is the Late Paleocene Plesiadapis, circa 55–58 million years old.[6] Molecular clock studies suggest that the primate branch may be even older, originating near the Cretaceous–Paleogene boundary or around 63-74 mya",
+		image1 : "./Projects/details/canny16.png",
+		image2 : "./Projects/details/cluster_results.png",
+		name : "Robotic Vision Performance Boost using a Pi Cluster",
+		description : "Write things here about it",
 		item: 7,
 		youtubelink: "https://www.youtube.com/embed/z9Ul9ccDOqE"
 	},
 	{
-		image1 : "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/%D0%9C%D1%8B%D1%88%D1%8C_2.jpg/500px-%D0%9C%D1%8B%D1%88%D1%8C_2.jpg",
-		image2 : "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Fancy_mice.jpg/500px-Fancy_mice.jpg",
-		name : "Mouse",
-		description : "A mouse (plural: mice) is a small rodent characteristically having a pointed snout, small rounded ears, a body-length scaly tail and a high breeding rate. The best known mouse species is the common house mouse (Mus musculus). It is also a popular pet. In some places, certain kinds of field mice are locally common. They are known to invade homes for food and shelter.",
+		image1 : "./Projects/details/drum_beat.gif",
+		image2 : "./Projects/details/vr.png",
+		name : "Virtual Reality Drumkit",
+		description : "We make drum!",
 		item: 8,
 		youtubelink: "https://www.youtube.com/embed/z9Ul9ccDOqE"
 	},
 	{
 		image1 : "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/American_Flamingo_-_Phoenicopterus_ruber.jpg/440px-American_Flamingo_-_Phoenicopterus_ruber.jpg",
 		image2 : "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Caribbean_flamingo.jpg/440px-Caribbean_flamingo.jpg",
-		name : "American flamingo",
+		name : "Chess AI",
 		description : "The American flamingo (Phoenicopterus ruber) is a large species of flamingo closely related to the greater flamingo and Chilean flamingo. It was formerly considered conspecific with the greater flamingo, but that treatment is now widely viewed (e.g. by the American and British Ornithologists' Unions) as incorrect due to a lack of evidence. It has also been known as the Caribbean flamingo, but the species' presence in the Galápagos makes that name problematic. It is the only flamingo which naturally inhabits North America",
 		item: 9,
 		youtubelink: "https://www.youtube.com/embed/z9Ul9ccDOqE"
 	},
 	{
-		image1 : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Papageitaucher_Fratercula_arctica.jpg/600px-Papageitaucher_Fratercula_arctica.jpg",
-		image2 : "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Atlantic_Puffin_Fratercula_arctica.jpg/440px-Atlantic_Puffin_Fratercula_arctica.jpg",
-		name : "Atlantic puffin",
-		description : "The Atlantic puffin (Fratercula arctica), also known as the common puffin, is a species of seabird in the auk family. It is the only puffin native to the Atlantic Ocean; two related species, the tufted puffin and the horned puffin, are found in the northeastern Pacific. The Atlantic puffin breeds in Iceland, Norway, Greenland, Newfoundland and many North Atlantic islands, and as far south as Maine in the west and the British Isles in the east. The Atlantic puffin has a large population and a wide range. It is not considered to be endangered although there may be local declines in numbers. On land, it has the typical upright stance of an auk. At sea, they swim on the surface and feed mainly on small fish, which they catch by diving underwater, using their wings for propulsion.",
+		image1 : "./Projects/details/Prediction_Full.png",
+		image2 : "./Projects/details/tracking.gif",
+		name : "Ball Catching Drone",
+		description : "The Nimbus lab spent some time this fall getting a drone to catch a ball. Although catching a ball for you or me is intuitive, getting a machine to do it requires a set of processors and sensors running together. The three main components we focused on to solve this problem were as follows: 1)Tracking and predicting where the ball will go. 2)Getting the drone to move in that direction aggressively. 3)Making sure the drone recovers from its aggressive movement safely. Tracking and predicting the movements of the ball was done using infrared cameras and a little bit of high school physics. Nimbus lab uses a motion capture system known as Vicon to track objects. This system uses infrared cameras to track the movements of reflectors which you can attach to objects. We attached the markers to the ball and that's all we needed to track the ball and its movements. Next was to predict where the ball was going. The basic idea to do this was to calculate the velocity of the ball in the x, y and z-direction. These velocities were then put into the equations of motion we learnt in high school physics, and now you have a rough idea of where the ball is going! Now that we know where the ball is going to be, we need to get the drone there as quickly as possible. Students at Nimbus lab have created their own drone control software. This system uses a position and velocity controller to take a drone from its current position to a new position. Without getting into too much detail, if you change these controllers’ parameters you are able to change the properties of how the drone gets there. We changed these parameters to make the drone minimize the time to get to the new point. Finally, you need to have software which makes sure the drone operates safely and is able to recover from any aggressive movements. These include things like checking if the drone missed the ball, caught the ball, or is still trying to catch the ball and then doing the appropriate behavior at this time. The initial version of our drone used a lightweight wood, known as balsa wood (which you find in your local hobby shops) to protect the drone from the ball. The next version of this drone will be made out of carbon fiber and custom 3D printed lightweight joints.",
+		image1_lab : "Visualization of the ball's trajectory. The blue line is the actual path, while the red line the predicted path using the 3 measurements marked as blue dots",
+		image2_lab : "Tracking",
 		item: 10,
-		youtubelink: "https://www.youtube.com/embed/z9Ul9ccDOqE"
+		youtubelink: "https://www.youtube.com/embed/WRHlGngb4tg"
 		},
 	{
 		image1 : "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/%C3%81guila_calva.jpg/440px-%C3%81guila_calva.jpg",
 		image2 : "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Golden_Eagle_in_flight_-_5.jpg/440px-Golden_Eagle_in_flight_-_5.jpg",
-		name : "Eagle",
+		name : "Robot Navigation",
 		description : "Eagle is a common name for many large birds of prey of the family Accipitridae; it belongs to several groups of genera that are not necessarily closely related to each other. Most of the 60 species of eagles are from Eurasia and Africa.[1] Outside this area, just 14 species can be found – two in North America, nine in Central and South America, and three in Australia.",
 		item: 11,
+		youtubelink: "https://www.youtube.com/embed/z9Ul9ccDOqE"
+	},
+	{
+		image1 : "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/%C3%81guila_calva.jpg/440px-%C3%81guila_calva.jpg",
+		image2 : "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Golden_Eagle_in_flight_-_5.jpg/440px-Golden_Eagle_in_flight_-_5.jpg",
+		name : "Ariel Vehicle Igintion Starter",
+		description : "Eagle is a common name for many large birds of prey of the family Accipitridae; it belongs to several groups of genera that are not necessarily closely related to each other. Most of the 60 species of eagles are from Eurasia and Africa.[1] Outside this area, just 14 species can be found – two in North America, nine in Central and South America, and three in Australia.",
+		item: 12,
 		youtubelink: "https://www.youtube.com/embed/z9Ul9ccDOqE"
 	}
 ]};
